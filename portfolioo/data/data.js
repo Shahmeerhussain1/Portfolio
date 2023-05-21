69ae7dd9eefd7d3a9139e7a99f65f7624ac1c221
Shahmeer Hussain <hussainshahmeer99@gmail.com>
2023-05-21 14:19:17 +0500

    Updates

---
+++ portfolioo/data/data.js
@@ -0,0 +1,10 @@
+const data = {
+    //SLOGAN 
+    slogan : 'I am a coder'
+
+
+
+    
+}
+
+export default data
---
+++ portfolioo/pages/index.js
@@ -0,0 +1,8 @@
+import data from '../data/data'
+export default function Home() {
+    return (
+      <h1>{data.slogan}</h1>
+     
+    )
+  }
+  

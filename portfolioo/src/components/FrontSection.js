4f12711262699c5dcc047a376026e45a14988c28
Shahmeer Hussain <hussainshahmeer99@gmail.com>
2023-05-28 16:18:03 +0500

    .

--- portfolioo/src/components/FrontSection.js
+++ portfolioo/src/components/FrontSection.js
@@ -1,22 +1,31 @@
 import data from "@/data/data";
 const FrontSection = () => {
-  console.log("data", (data.THEME.toLocaleLowerCase() == "black" ||
-  data.THEME.toLocaleLowerCase() == "white")
-  ? `${data.THEME}-theme`
-  : "black-theme");
+  console.log(
+    "data",
+    data.THEME.toLocaleLowerCase() == "black" ||
+      data.THEME.toLocaleLowerCase() == "white"
+      ? `${data.THEME}-theme`
+      : "black-theme"
+  );
   return (
     <>
-      <div id="frontpage-main">
-        <div
-          className={
-            data.THEME &&
-            (data.THEME.toLocaleLowerCase() == "black" ||
-              data.THEME.toLocaleLowerCase() == "white")
-              ? `${data.THEME}-theme`
-              : "black-theme"
-          }
-        >
-          Shahmeer
+      <div
+        id={
+          data.THEME &&
+          (data.THEME.toLocaleLowerCase() == "black" ||
+            data.THEME.toLocaleLowerCase() == "white")
+            ? `${data.THEME}-theme-frontPage`
+            : "black-theme-frontPage"
+        }
+      >
+        <div className="frontMain">
+
+
+          <div className="frontPagethreenameMain">
+            <div className="mainGreeting">Hi, i am</div>
+            <div className="mainName">{data.NAME ? data.NAME : "Shahmeer"}</div>
+            <div className="mainRole">{`a ${data.ROLE ? data.ROLE : "a developer"}`}</div>
+          </div>
         </div>
       </div>
     </>

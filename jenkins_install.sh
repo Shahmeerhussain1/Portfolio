5cbb3450632115256789d740ffc5d5b1b28aedce
Shah Meer Hussain <97156017+Shahmeerhussain1@users.noreply.github.com>
2025-04-07 15:42:16 +0500

    Update jenkins_install.sh

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -4,6 +4,6 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt install fontconfig openjdk-17-jre
-sudo apt-get install jenkins
+sudo apt-get update > /dev/null
+sudo apt install fontconfig openjdk-17-jre > /dev/null
+sudo apt-get install jenkins > /dev/null
